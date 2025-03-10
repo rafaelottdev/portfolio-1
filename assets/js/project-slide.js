d1498e64731cb670e4430d53db1ab4a1722a27c7
Rafael Ott Alves <157972006+rafaelottdev@users.noreply.github.com>
2025-03-10 20:26:07 -0300

    feat: add-js-4

--- assets/js/project-slide.js
+++ assets/js/project-slide.js
@@ -1,34 +1,46 @@
 const slideBtnList = [... document.querySelectorAll('.slide-btn')]
 
-function toLeft() {
+let currentSlideValue = 350
 
-}
-
-function toRight() {
-    
-}
-
-function identifyClickedBtn(event) {
+function getClassBtn(event) {
     const btn = event.target
     let btnClassList = btn.className
 
-    if(btnClassList.animVal == "svg-btn") {
+    if (btnClassList.animVal == "svg-btn") {
         btnClassList = btn.parentNode.className
     }
 
-    const btnClass = btnClassList.split(" ")[0]
+    else if (btnClassList.animVal == "") {
+        btnClassList = btn.parentNode.parentNode.className
+    }
+    
+    return btnClass = btnClassList.split(" ")[0]
+}
 
-    switch (btnClass) {
+function whatBtn(clickedClass) {
+    let btnSide = ""
+
+    switch (clickedClass) {
         case "project-slide-container__left-btn":
-            toLeft()
+            btnSide = 'esquerdo'
             break
-
-        case "roject-slide-container__right-btn":
-            toRight()
+    
+        case "project-slide-container__right-btn":
+            btnSide = 'direito'
             break
     }
+
+    return btnSide
+}
+
+function clickedButton(event) {
+    const clickedClass = getClassBtn(event)
+
+    const btn = whatBtn(clickedClass)
 }
 
 slideBtnList.forEach((btn) => {
-    btn.addEventListener('click',identifyClickedBtn )
+    btn.addEventListener('click',clickedButton)
 })
+
+// const projectList = document.querySelector('.container_project-list')
